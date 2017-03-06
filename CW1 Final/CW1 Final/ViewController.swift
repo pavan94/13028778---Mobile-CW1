0f68e00b0345b3f08ea08feb2626e5042d7f9571
pavan94 <pavz94@hotmail.co.uk>
2017-03-06 15:19:21 +0000

    Add files via upload

--- CW1 Final/CW1 Final/ViewController.swift
+++ CW1 Final/CW1 Final/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    // Buttons
     @IBOutlet weak var Button0: UIButton!
     @IBOutlet weak var Button1: UIButton!
     @IBOutlet weak var Button2: UIButton!
@@ -23,6 +23,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var Button9: UIButton!
     @IBOutlet weak var Next: UIButton!
     
+    // Labels
     @IBOutlet weak var QuestionLabel: UILabel!
     
     var Answer = String()
@@ -40,9 +41,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
+    // Function to generate and randomize questions
     func Randomize(){
-        var RandomNumber = arc4random() % 4
+        var RandomNumber = arc4random() % 15
         RandomNumber += 1
         
         switch(RandomNumber){
@@ -109,6 +110,193 @@ class ViewController: UIViewController {
             Button8.setTitle("8", for: UIControlState())
             Button9.setTitle("9", for: UIControlState())
             Answer = "9"
+            
+            break
+            
+        case 5:
+            
+            QuestionLabel.text = "4 + 3 = ?"
+            Button0.setTitle("0", for: UIControlState())
+            Button1.setTitle("1", for: UIControlState())
+            Button2.setTitle("2", for: UIControlState())
+            Button3.setTitle("3", for: UIControlState())
+            Button4.setTitle("4", for: UIControlState())
+            Button5.setTitle("5", for: UIControlState())
+            Button6.setTitle("6", for: UIControlState())
+            Button7.setTitle("7", for: UIControlState())
+            Button8.setTitle("8", for: UIControlState())
+            Button9.setTitle("9", for: UIControlState())
+            Answer = "7"
+            
+            break
+            
+        case 6:
+            
+            QuestionLabel.text = "3 + 2 = ?"
+            Button0.setTitle("0", for: UIControlState())
+            Button1.setTitle("1", for: UIControlState())
+            Button2.setTitle("2", for: UIControlState())
+            Button3.setTitle("3", for: UIControlState())
+            Button4.setTitle("4", for: UIControlState())
+            Button5.setTitle("5", for: UIControlState())
+            Button6.setTitle("6", for: UIControlState())
+            Button7.setTitle("7", for: UIControlState())
+            Button8.setTitle("8", for: UIControlState())
+            Button9.setTitle("9", for: UIControlState())
+            Answer = "5"
+            
+            break
+            
+        case 7:
+            
+            QuestionLabel.text = "5 + 2 = ?"
+            Button0.setTitle("0", for: UIControlState())
+            Button1.setTitle("1", for: UIControlState())
+            Button2.setTitle("2", for: UIControlState())
+            Button3.setTitle("3", for: UIControlState())
+            Button4.setTitle("4", for: UIControlState())
+            Button5.setTitle("5", for: UIControlState())
+            Button6.setTitle("6", for: UIControlState())
+            Button7.setTitle("7", for: UIControlState())
+            Button8.setTitle("8", for: UIControlState())
+            Button9.setTitle("9", for: UIControlState())
+            Answer = "7"
+            
+            break
+            
+        case 8:
+            
+            QuestionLabel.text = "1 + 1 = ?"
+            Button0.setTitle("0", for: UIControlState())
+            Button1.setTitle("1", for: UIControlState())
+            Button2.setTitle("2", for: UIControlState())
+            Button3.setTitle("3", for: UIControlState())
+            Button4.setTitle("4", for: UIControlState())
+            Button5.setTitle("5", for: UIControlState())
+            Button6.setTitle("6", for: UIControlState())
+            Button7.setTitle("7", for: UIControlState())
+            Button8.setTitle("8", for: UIControlState())
+            Button9.setTitle("9", for: UIControlState())
+            Answer = "2"
+            
+            break
+            
+        case 9:
+            
+            QuestionLabel.text = "0 + 6 = ?"
+            Button0.setTitle("0", for: UIControlState())
+            Button1.setTitle("1", for: UIControlState())
+            Button2.setTitle("2", for: UIControlState())
+            Button3.setTitle("3", for: UIControlState())
+            Button4.setTitle("4", for: UIControlState())
+            Button5.setTitle("5", for: UIControlState())
+            Button6.setTitle("6", for: UIControlState())
+            Button7.setTitle("7", for: UIControlState())
+            Button8.setTitle("8", for: UIControlState())
+            Button9.setTitle("9", for: UIControlState())
+            Answer = "6"
+            
+            break
+            
+        case 10:
+            
+            QuestionLabel.text = "4 + 4 = ?"
+            Button0.setTitle("0", for: UIControlState())
+            Button1.setTitle("1", for: UIControlState())
+            Button2.setTitle("2", for: UIControlState())
+            Button3.setTitle("3", for: UIControlState())
+            Button4.setTitle("4", for: UIControlState())
+            Button5.setTitle("5", for: UIControlState())
+            Button6.setTitle("6", for: UIControlState())
+            Button7.setTitle("7", for: UIControlState())
+            Button8.setTitle("8", for: UIControlState())
+            Button9.setTitle("9", for: UIControlState())
+            Answer = "8"
+            
+            break
+            
+        case 11:
+            
+            QuestionLabel.text = "5 + 2 = ?"
+            Button0.setTitle("0", for: UIControlState())
+            Button1.setTitle("1", for: UIControlState())
+            Button2.setTitle("2", for: UIControlState())
+            Button3.setTitle("3", for: UIControlState())
+            Button4.setTitle("4", for: UIControlState())
+            Button5.setTitle("5", for: UIControlState())
+            Button6.setTitle("6", for: UIControlState())
+            Button7.setTitle("7", for: UIControlState())
+            Button8.setTitle("8", for: UIControlState())
+            Button9.setTitle("9", for: UIControlState())
+            Answer = "7"
+            
+            break
+            
+        case 12:
+            
+            QuestionLabel.text = "9 + 0 = ?"
+            Button0.setTitle("0", for: UIControlState())
+            Button1.setTitle("1", for: UIControlState())
+            Button2.setTitle("2", for: UIControlState())
+            Button3.setTitle("3", for: UIControlState())
+            Button4.setTitle("4", for: UIControlState())
+            Button5.setTitle("5", for: UIControlState())
+            Button6.setTitle("6", for: UIControlState())
+            Button7.setTitle("7", for: UIControlState())
+            Button8.setTitle("8", for: UIControlState())
+            Button9.setTitle("9", for: UIControlState())
+            Answer = "9"
+            
+            break
+            
+        case 13:
+            
+            QuestionLabel.text = "7 + 2  = ?"
+            Button0.setTitle("0", for: UIControlState())
+            Button1.setTitle("1", for: UIControlState())
+            Button2.setTitle("2", for: UIControlState())
+            Button3.setTitle("3", for: UIControlState())
+            Button4.setTitle("4", for: UIControlState())
+            Button5.setTitle("5", for: UIControlState())
+            Button6.setTitle("6", for: UIControlState())
+            Button7.setTitle("7", for: UIControlState())
+            Button8.setTitle("8", for: UIControlState())
+            Button9.setTitle("9", for: UIControlState())
+            Answer = "9"
+            
+            break
+            
+        case 14:
+            
+            QuestionLabel.text = "8 + 0 = ?"
+            Button0.setTitle("0", for: UIControlState())
+            Button1.setTitle("1", for: UIControlState())
+            Button2.setTitle("2", for: UIControlState())
+            Button3.setTitle("3", for: UIControlState())
+            Button4.setTitle("4", for: UIControlState())
+            Button5.setTitle("5", for: UIControlState())
+            Button6.setTitle("6", for: UIControlState())
+            Button7.setTitle("7", for: UIControlState())
+            Button8.setTitle("8", for: UIControlState())
+            Button9.setTitle("9", for: UIControlState())
+            Answer = "8"
+            
+            break
+            
+        case 15:
+            
+            QuestionLabel.text = "2 + 6 = ?"
+            Button0.setTitle("0", for: UIControlState())
+            Button1.setTitle("1", for: UIControlState())
+            Button2.setTitle("2", for: UIControlState())
+            Button3.setTitle("3", for: UIControlState())
+            Button4.setTitle("4", for: UIControlState())
+            Button5.setTitle("5", for: UIControlState())
+            Button6.setTitle("6", for: UIControlState())
+            Button7.setTitle("7", for: UIControlState())
+            Button8.setTitle("8", for: UIControlState())
+            Button9.setTitle("9", for: UIControlState())
+            Answer = "8"
             
             break
             
