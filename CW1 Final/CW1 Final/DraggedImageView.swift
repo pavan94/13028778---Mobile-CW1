efe47f82f12f055cc00f31df66218109c663bcae
pavan94 <pavz94@hotmail.co.uk>
2017-03-06 14:36:28 +0000

    Add files via upload

---
+++ CW1 Final/CW1 Final/DraggedImageView.swift
@@ -0,0 +1,38 @@
+//
+//  DraggedImageView.swift
+//  CW1 Final
+//
+//  Created by Pavan Patel on 06/03/2017.
+//  Copyright © 2017 Pavan Patel. All rights reserved.
+//
+
+import UIKit
+
+class DraggedImageView: UIImageView {
+    
+    var startLocation: CGPoint?
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        startLocation = touches.first?.location(in: self)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let currentLocation = touches.first?.location(in: self)
+        let dx = currentLocation!.x - startLocation!.x
+        let dy = currentLocation!.y - startLocation!.y
+        
+        self.center = CGPoint(x: self.center.x+dx, y: self.center.y+dy)
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CW1 Final/CW1 Final/ViewController.swift
@@ -0,0 +1,255 @@
+//
+//  ViewController.swift
+//  CW1 Final
+//
+//  Created by Pavan Patel on 06/03/2017.
+//  Copyright © 2017 Pavan Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var Button0: UIButton!
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
+    @IBOutlet weak var Button4: UIButton!
+    @IBOutlet weak var Button5: UIButton!
+    @IBOutlet weak var Button6: UIButton!
+    @IBOutlet weak var Button7: UIButton!
+    @IBOutlet weak var Button8: UIButton!
+    @IBOutlet weak var Button9: UIButton!
+    @IBOutlet weak var Next: UIButton!
+    
+    @IBOutlet weak var QuestionLabel: UILabel!
+    
+    var Answer = String()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        Randomize()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func Randomize(){
+        var RandomNumber = arc4random() % 4
+        RandomNumber += 1
+        
+        switch(RandomNumber){
+            
+        case 1:
+            
+            QuestionLabel.text = "3 + 1 = ?"
+            Button0.setTitle("0", for: UIControlState())
+            Button1.setTitle("1", for: UIControlState())
+            Button2.setTitle("2", for: UIControlState())
+            Button3.setTitle("3", for: UIControlState())
+            Button4.setTitle("4", for: UIControlState())
+            Button5.setTitle("5", for: UIControlState())
+            Button6.setTitle("6", for: UIControlState())
+            Button7.setTitle("7", for: UIControlState())
+            Button8.setTitle("8", for: UIControlState())
+            Button9.setTitle("9", for: UIControlState())
+            Answer = "4"
+            
+            break
+        case 2:
+            
+            QuestionLabel.text = "4 + 2 = ?"
+            Button0.setTitle("0", for: UIControlState())
+            Button1.setTitle("1", for: UIControlState())
+            Button2.setTitle("2", for: UIControlState())
+            Button3.setTitle("3", for: UIControlState())
+            Button4.setTitle("4", for: UIControlState())
+            Button5.setTitle("5", for: UIControlState())
+            Button6.setTitle("6", for: UIControlState())
+            Button7.setTitle("7", for: UIControlState())
+            Button8.setTitle("8", for: UIControlState())
+            Button9.setTitle("9", for: UIControlState())
+            Answer = "6"
+            
+            break
+        case 3:
+            
+            QuestionLabel.text = "5 + 3 = ?"
+            Button0.setTitle("0", for: UIControlState())
+            Button1.setTitle("1", for: UIControlState())
+            Button2.setTitle("2", for: UIControlState())
+            Button3.setTitle("3", for: UIControlState())
+            Button4.setTitle("4", for: UIControlState())
+            Button5.setTitle("5", for: UIControlState())
+            Button6.setTitle("6", for: UIControlState())
+            Button7.setTitle("7", for: UIControlState())
+            Button8.setTitle("8", for: UIControlState())
+            Button9.setTitle("9", for: UIControlState())
+            Answer = "8"
+            
+            break
+        case 4:
+            
+            QuestionLabel.text = "8 + 1 = ?"
+            Button0.setTitle("0", for: UIControlState())
+            Button1.setTitle("1", for: UIControlState())
+            Button2.setTitle("2", for: UIControlState())
+            Button3.setTitle("3", for: UIControlState())
+            Button4.setTitle("4", for: UIControlState())
+            Button5.setTitle("5", for: UIControlState())
+            Button6.setTitle("6", for: UIControlState())
+            Button7.setTitle("7", for: UIControlState())
+            Button8.setTitle("8", for: UIControlState())
+            Button9.setTitle("9", for: UIControlState())
+            Answer = "9"
+            
+            break
+            
+        default:
+            break
+        }
+    }
+    
+    
+    func Hide() {
+        
+        Next.isHidden = true
+        
+        
+    }
+    
+    func Unhide() {
+        
+        Next.isHidden = false
+        
+    }
+
+    
+    
+    @IBAction func B0Action(_ sender: AnyObject) {
+        
+        if Answer == "0" {
+            self.performSegue(withIdentifier: "correctSegue", sender: nil)
+        }
+        else {
+            self.performSegue(withIdentifier: "incorrectSegue", sender: nil)
+        }
+
+    }
+    
+    @IBAction func B1Action(_ sender: AnyObject) {
+        
+        if Answer == "1" {
+            self.performSegue(withIdentifier: "correctSegue", sender: nil)
+        }
+        else {
+            self.performSegue(withIdentifier: "incorrectSegue", sender: nil)
+        }
+
+    }
+    
+    @IBAction func B2Action(_ sender: AnyObject) {
+        
+        if Answer == "2" {
+            self.performSegue(withIdentifier: "correctSegue", sender: nil)
+        }
+        else {
+            self.performSegue(withIdentifier: "incorrectSegue", sender: nil)
+        }
+
+    }
+    
+    @IBAction func B3Action(_ sender: AnyObject) {
+        
+        if Answer == "3" {
+            self.performSegue(withIdentifier: "correctSegue", sender: nil)
+        }
+        else {
+            self.performSegue(withIdentifier: "incorrectSegue", sender: nil)
+        }
+
+    }
+
+    @IBAction func B4Action(_ sender: AnyObject) {
+        
+        if Answer == "4" {
+            self.performSegue(withIdentifier: "correctSegue", sender: nil)
+        }
+        else {
+            self.performSegue(withIdentifier: "incorrectSegue", sender: nil)
+        }
+
+    }
+
+    @IBAction func B5Action(_ sender: AnyObject) {
+        
+        if Answer == "5" {
+            self.performSegue(withIdentifier: "correctSegue", sender: nil)
+        }
+        else {
+            self.performSegue(withIdentifier: "incorrectSegue", sender: nil)
+        }
+
+    }
+    
+    @IBAction func B6Action(_ sender: AnyObject) {
+        
+        if Answer == "6" {
+            self.performSegue(withIdentifier: "correctSegue", sender: nil)
+        }
+        else {
+            self.performSegue(withIdentifier: "incorrectSegue", sender: nil)
+        }
+
+    }
+    
+    @IBAction func B7Action(_ sender: AnyObject) {
+        
+        if Answer == "7" {
+            self.performSegue(withIdentifier: "correctSegue", sender: nil)
+        }
+        else {
+            self.performSegue(withIdentifier: "incorrectSegue", sender: nil)
+        }
+
+    }
+    
+    @IBAction func B8Action(_ sender: AnyObject) {
+        
+        if Answer == "8" {
+            self.performSegue(withIdentifier: "correctSegue", sender: nil)
+        }
+        else {
+            self.performSegue(withIdentifier: "incorrectSegue", sender: nil)
+        }
+
+    }
+    
+    @IBAction func B9Action(_ sender: AnyObject) {
+        
+        if Answer == "9" {
+            self.performSegue(withIdentifier: "correctSegue", sender: nil)
+        }
+        else {
+            self.performSegue(withIdentifier: "incorrectSegue", sender: nil)
+        }
+
+    }
+    
+    
+    @IBAction func Next(_ sender: AnyObject) {
+        
+        Randomize()
+    }
+    
+}
+
+
+
